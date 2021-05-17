f6cf2d98475451a7709b4054f387e50047e18f3f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-17 22:35:33 +0800

    Platina: Match stock model name
    Change-Id: I8a1f285e366aaf094169d7bb3bb06170b36da4cb

--- exthm_platina.mk
+++ exthm_platina.mk
@@ -33,7 +33,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := exthm_platina
-PRODUCT_MODEL := Mi 8 Lite
+PRODUCT_MODEL := MI 8 Lite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
